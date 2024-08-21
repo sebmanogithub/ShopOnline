c5dce166ab73a209549cd551f4cdbcd5a2f9a56a
sebmanogithub <sebastiengithub@gmail.com>
2024-08-21 06:20:32 +0200

    Ajout d'un projet model pour création des classes DTO

---
+++ ShopOnline.Models/Dtos/ProductCategoryDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models.Dtos
+{
+    public class ProductCategoryDto
+    {
+        public ProductCategoryDto(int id, string name, string iconCss)
+        {
+            Id = id;
+            Name = name;
+            IconCSS = iconCss;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconCSS { get; set; }
+    }
+}
--- ShopOnline.Models/obj/Debug/net6.0/ShopOnline.Models.AssemblyInfo.cs
+++ ShopOnline.Models/obj/Debug/net6.0/ShopOnline.Models.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ShopOnline.Api")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ShopOnline.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ShopOnline.Api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ShopOnline.Api")]
+[assembly: System.Reflection.AssemblyProductAttribute("ShopOnline.Models")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ShopOnline.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
