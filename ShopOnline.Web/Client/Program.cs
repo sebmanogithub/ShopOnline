97801438cc787c4296dbebf0f0154f4f5c3985b8
sebmanogithub <sebastiengithub@gmail.com>
2024-08-22 07:14:24 +0200

    Creation de la rcuperation des donnes de l'API dans le client via ProductService

--- ShopOnline.Web/Client/Program.cs
+++ ShopOnline.Web/Client/Program.cs
@@ -12,7 +12,8 @@ namespace ShopOnline.Web
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient 
+            { BaseAddress = new Uri("https://localhost:7134/") });
 
             await builder.Build().RunAsync();
         }
---
+++ ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public HttpClient HttpClient { get; }
+
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                var products = await httpClient
+                    .GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+                return products;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
