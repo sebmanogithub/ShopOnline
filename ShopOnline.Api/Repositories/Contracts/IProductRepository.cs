92fac6a785c2b7d7f6fa3b4ef68bb2c3bcd1c3c5
sebmanogithub <sebastiengithub@gmail.com>
2024-08-22 05:33:33 +0200

    Conversion en Dto

--- ShopOnline.Api/Repositories/Contracts/IProductRepository.cs
+++ ShopOnline.Api/Repositories/Contracts/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace ShopOnline.Api.Repository.Contracts
+﻿using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Repositories.Contracts
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<IEnumerable<Product>> GetItem(int id);
+        Task<IEnumerable<ProductCategory>> GetCategory(int id);
     }
 }
